8a8c94e0bce0781d4fc9af30073e097c33d0f24a
ZeliTheZealot <jerrybenjaminw@gmail.com>
2023-06-15 10:43:06 +0200

    initial mine

---
+++ chapter0_fundamentals/exercises/part1_ray_tracing/typing_demo.py
@@ -0,0 +1,15 @@
+#%%
+from jaxtyping import Float, Int, Bool, Shaped, jaxtyped
+import typeguard
+import torch as t
+from torch import Tensor
+#%%
+@jaxtyped
+@typeguard.typechecked
+def my_concat(x: Float[Tensor, "a1 b"], y: Float[Tensor, "a2 b"]) -> Float[Tensor, "a1+a2 b"]:
+    return t.concat([x, y], dim=0)
+
+x = t.ones(3, 2)
+y = t.randn(4, 2)
+z = my_concat(x, y)
+# %%
