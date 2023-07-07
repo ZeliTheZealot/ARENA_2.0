8f328f9536720a3168076715bc83938c7515ff37
ZeliTheZealot <jerrybenjaminw@gmail.com>
2023-07-07 16:22:30 +0000

    stuff

--- chapter0_fundamentals/exercises/part3_resnets/answers.py
+++ chapter0_fundamentals/exercises/part3_resnets/answers.py
@@ -71,11 +71,6 @@ if MAIN:
     print(summary)
 
 # %%
-
-x = t.rand((1, 1, 28, 28)).to(device)
-model(x)
-print([x.device for x in model.parameters()])
-# %%
 if MAIN:
     MNIST_TRANSFORM = transforms.Compose([
         transforms.ToTensor(),
@@ -509,10 +504,6 @@ class ResNet34(nn.Module):
 
 if MAIN:
     my_resnet = ResNet34()
-# %%
-
-x = t.rand((1, 3, 224, 224))
-my_resnet(x)
 
 # %%
 def copy_weights(my_resnet: ResNet34, pretrained_resnet: models.resnet.ResNet) -> ResNet34:
@@ -539,8 +530,7 @@ def copy_weights(my_resnet: ResNet34, pretrained_resnet: models.resnet.ResNet) -
 if MAIN:
     pretrained_resnet = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1)
     my_resnet = copy_weights(my_resnet, pretrained_resnet)
-# %%
-print_param_count(my_resnet, pretrained_resnet)
+
 # %%
 if MAIN:
     IMAGE_FILENAMES = [
@@ -561,7 +551,7 @@ if MAIN:
     images = [Image.open(IMAGE_FOLDER / filename) for filename in IMAGE_FILENAMES]
 # %%
 # if MAIN:
-images[0]
+# images[0]
 # %%
 IMAGE_SIZE = 224
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
